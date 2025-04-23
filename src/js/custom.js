9015361fb483ca6fdeea063774f32c986f683085
oscarjavier12 <soy767mex@gmail.com>
2025-04-22 23:28:14 -0600

    a

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,9 @@
-const contenedor = document.querySelector('.container');
+const contenedor = document.querySelector('.contenido');
 const elementos = document.querySelector('.elementos');
 const flechaIzquierda = document.querySelector('.izquierda');
 const flechaDerecha = document.querySelector('.derecha');
 
 let posicion = 0; // Posición actual del scroll
-
 flechaIzquierda.addEventListener('click', () => {
     posicion -= 100; // Ajusta la cantidad de scroll
     if (posicion < 0) {
--- src/js/inicio.js
+++ src/js/inicio.js
@@ -1,7 +1,37 @@
+/**
 document.addEventListener('DOMContentLoaded', function () {
     if (sessionStorage.getItem('isLoggedIn') !== 'true') {
         window.location.href = "login.html"; // Redirige al login si no está autenticado
         return; // evita que el resto del código se ejecute
     }
+        
 });
+*/
+
+const menu = document.getElementsByClassName("menu");
+menu[0].addEventListener("click", function () {
+    cargarContenido("/src/html/Inicio.html");
+});
+menu[1].addEventListener("click", function () {
+    cargarContenido("/src/html/Informacion.html");
+});
+menu[2].addEventListener("click", function () {
+    cargarContenido("/src/html/Productos.html");
+});
+menu[3].addEventListener("click", function () {
+    cargarContenido("/src/html/Contacto.html");
+});
+
+function cargarContenido(ruta) {
+    fetch(ruta)
+        .then(response => response.text()) // Convierte la respuesta a texto
+        .then(html => {
+            document.getElementById('contenedor').innerHTML = html; // Inserta el HTML en un elemento existente
+            
+        })
+        .catch(error => {
+            console.error('Error al cargar el HTML:', error);
+        });
+};
+
 
