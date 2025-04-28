fc6aa7483b2129701f4d6540b5683749ec9a6cb7
oscarjavier12 <soy767mex@gmail.com>
2025-04-28 09:20:36 -0600

    a

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,117 +1,116 @@
 import buttons from "./buttons.js";
-document.addEventListener('DOMContentLoaded', function () {
-    const rango = document.getElementById('productCantidad');
-    rango.addEventListener('input', function () {
-        // net es el siguiente elemento
-        this.nextElementSibling.value = this.value;
-    });
-    let datosCeldas = [];
+const rango = document.getElementById('productCantidad');
+rango.addEventListener('input', function () {
+    // net es el siguiente elemento
+    this.nextElementSibling.value = this.value;
+});
 
-    const cuerpo = document.getElementById('productTable')
-    cuerpo.addEventListener('click', function (event) {
+let datosCeldas = [];
+ocultarTablaProductos();
+const cuerpo = document.getElementById('productTable')
+cuerpo.addEventListener('click', function (event) {
 
-        if (event.target.id === 'btnEditar') {
-            const rowEditar = event.target.closest('tr');
-            const cells = rowEditar.querySelectorAll('td');
-            datosCeldas = [];
+    if (event.target.id === 'btnEditar') {
+        const rowEditar = event.target.closest('tr');
+        const cells = rowEditar.querySelectorAll('td');
+        datosCeldas = [];
 
-            cells.forEach((cell, index) => {
-                if (index < cells.length - 1) {
-                    const valorcelda = cell.textContent;
-                    datosCeldas.push(valorcelda);
+        cells.forEach((cell, index) => {
+            if (index < cells.length - 1) {
+                const valorcelda = cell.textContent;
+                datosCeldas.push(valorcelda);
 
-                    const input = document.createElement('input');
-                    input.type = 'text';
-                    input.value = valorcelda;
-                    input.classList = 'form-control';
-                    cell.textContent = "";
-                    cell.appendChild(input);
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = valorcelda;
+                input.classList = 'form-control';
+                cell.textContent = "";
+                cell.appendChild(input);
 
-                }
-            });
+            }
+        });
 
-            buttons.cambioBoton(
-                event,
-                buttons.botones.btnSave.id,
-                buttons.botones.btnSave.ruta,
-                buttons.botones.btnSave.title
-            );
-            const deleteButton = rowEditar.querySelector('#btnEliminar');
-            buttons.cambioBtnSinEvent(
-                deleteButton,
-                buttons.botones.btnCancel.id,
-                buttons.botones.btnCancel.ruta,
-                buttons.botones.btnCancel.title
-            );
+        buttons.cambioBoton(
+            event,
+            buttons.botones.btnSave.id,
+            buttons.botones.btnSave.ruta,
+            buttons.botones.btnSave.title
+        );
+        const deleteButton = rowEditar.querySelector('#btnEliminar');
+        buttons.cambioBtnSinEvent(
+            deleteButton,
+            buttons.botones.btnCancel.id,
+            buttons.botones.btnCancel.ruta,
+            buttons.botones.btnCancel.title
+        );
 
+        return;
+    }
+    if (event.target.id === 'btnGuardar') {
+        const rowSave = event.target.closest('tr');
+        const cajitaTexto = rowSave.querySelectorAll('input');
+
+        cajitaTexto.forEach((input, index) => {
+            if (index < cajitaTexto.length) {
+                const valorNuevo = input.value;
+                input.parentNode.textContent = valorNuevo;
+            }
+        });
+
+        buttons.cambioBoton(
+            event,
+            buttons.botones.btnEdit.id,
+            buttons.botones.btnEdit.ruta,
+            buttons.botones.btnEdit.title
+        );
+        const cancelarBoton = rowSave.querySelector('#btnCancelar');
+        buttons.cambioBtnSinEvent(
+            cancelarBoton,
+            buttons.botones.btnDelete.id,
+            buttons.botones.btnDelete.ruta,
+            buttons.botones.btnDelete.title
+        );
+
+        return;
+    };
+    if (event.target.id === 'btnCancelar') {
+        const rowCancel = event.target.closest('tr');
+        const cells = rowCancel.querySelectorAll('input');
+        if (datosCeldas.length != cells.length) {
+            console.log('Error: No se puede cancelar la edición');
             return;
         }
-        if (event.target.id === 'btnGuardar') {
-            const rowSave = event.target.closest('tr');
-            const cajitaTexto = rowSave.querySelectorAll('input');
-
-            cajitaTexto.forEach((input, index) => {
-                if (index < cajitaTexto.length) {
-                    const valorNuevo = input.value;
-                    input.parentNode.textContent = valorNuevo;
-                }
-            });
-
-            buttons.cambioBoton(
-                event,
-                buttons.botones.btnEdit.id,
-                buttons.botones.btnEdit.ruta,
-                buttons.botones.btnEdit.title
-            );
-            const cancelarBoton = rowSave.querySelector('#btnCancelar');
-            buttons.cambioBtnSinEvent(
-                cancelarBoton,
-                buttons.botones.btnDelete.id,
-                buttons.botones.btnDelete.ruta,
-                buttons.botones.btnDelete.title
-            );
-
-            return;
-        };
-        if (event.target.id === 'btnCancelar') {
-            const rowCancel = event.target.closest('tr');
-            const cells = rowCancel.querySelectorAll('input');
-            if (datosCeldas.length != cells.length) {
-                console.log('Error: No se puede cancelar la edición');
-                return;
+        cells.forEach((cell, index) => {
+            if (index < cells.length) {
+                const valorcelda = datosCeldas[index];
+                cell.parentNode.textContent = valorcelda;
             }
-            cells.forEach((cell, index) => {
-                if (index < cells.length) {
-                    const valorcelda = datosCeldas[index];
-                    cell.parentNode.textContent = valorcelda;
-                }
 
-            });
-            buttons.cambioBoton(
-                event,
-                buttons.botones.btnDelete.id,
-                buttons.botones.btnDelete.ruta,
-                buttons.botones.btnDelete.title
+        });
+        buttons.cambioBoton(
+            event,
+            buttons.botones.btnDelete.id,
+            buttons.botones.btnDelete.ruta,
+            buttons.botones.btnDelete.title
 
-            );
-            const guardarBoton = rowCancel.querySelector('#btnGuardar');
-            buttons.cambioBtnSinEvent(
-                guardarBoton,
-                buttons.botones.btnEdit.id,
-                buttons.botones.btnEdit.ruta,
-                buttons.botones.btnEdit.title
-            );
-            return;
+        );
+        const guardarBoton = rowCancel.querySelector('#btnGuardar');
+        buttons.cambioBtnSinEvent(
+            guardarBoton,
+            buttons.botones.btnEdit.id,
+            buttons.botones.btnEdit.ruta,
+            buttons.botones.btnEdit.title
+        );
+        return;
+    }
+    if (event.target.id.startsWith('btnEliminar')) {
+        const rowDelete = event.target.closest('tr');
+        rowDelete.remove();
+
+        if (cuerpo.children.length === 0) {
+            ocultarTablaProductos();
         }
-        if (event.target.id.startsWith('btnEliminar')) {
-            const rowDelete = event.target.closest('tr');
-            rowDelete.remove();
-
-            if (cuerpo.children.length === 0) {
-                ocultarTablaProductos();
-            }
-        };
-    });
+    };
 });
 
 function ocultarTablaProductos() {
@@ -145,7 +144,7 @@ form.addEventListener('submit', function (event) {
 })
 
 const agregarFIlaTabla = (formData, tbody) => {
-    
+
     const fila = document.createElement('tr');
     agregarCelda(fila, formData.get('productName'));
     agregarCelda(fila, formData.get('productCategory'));
